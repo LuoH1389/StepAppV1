709ac50654c040cd09846d4483f42aef2af13d8a
Geoff Hao <geoffhao@Geoffs-MacBook-Pro.local>
2023-10-22 04:56:30 +0200

    Initial commit

---
+++ app/src/main/java/com/example/stepappv1/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.stepappv1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private int counter = 0;
+    private TextView showCount;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        showCount = (TextView)findViewById(R.id.show_count);
+    }
+
+    public void showToast(View view) {
+        counter = 0;
+        showCount.setText(Integer.toString(counter));
+        Toast toast = Toast.makeText (this,R.string.start, Toast.LENGTH_LONG);
+        toast.show();
+    }
+
+    public void countUp(View view) {
+        counter ++;
+        showCount.setText(Integer.toString(counter));
+    }
+}
